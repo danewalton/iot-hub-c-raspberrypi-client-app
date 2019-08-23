6a0fc5a449fec76ac606bd0b890e0923506182fb
Dane Walton <dawalton@microsoft.com>
2019-08-23 15:34:02 -0700

    fix build script

--- setup.sh
+++ setup.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
-sh -c '
+
 #On Raspbian Stretch we need to install old libssl (1.02 to be exact)
 sudo apt-get update
 sudo apt-get purge -y libssl-dev
 sudo apt-get install -y libssl1.0-dev git cmake build-essential curl libcurl4-openssl-dev uuid-dev
 
 #On Raspbian Stretch precompiled sdk from ppa is no good. Lets compile it manually
-cd ~
-mkdir Source
-cd Source
-git clone --recursive https://github.com/azure/azure-iot-sdk-c.git
+pushd ../
+if [ -d azure-iot-sample-source/azure-iot-sdk-c ]
+then
+    echo "Azure IoT repo already downloaded. Checking for submodules"
+    cd azure-iot-sample-source/azure-iot-sdk-c/
+    git pull origin master
+    git submodule update --init --recursive
+    cd ..
+else
+    echo "Downloading Azure IoT repo"
+    mkdir azure-iot-sample-source
+    cd azure-iot-sample-source
+    git clone --recursive https://github.com/Azure/azure-iot-sdk-c.git
+fi
 
-cd azure-iot-sdk-c/build_all/linux
+pushd azure-iot-sdk-c/build_all/linux
 ./build.sh --no-make
 
-cd ../../cmake/iotsdk_linux
+pushd ../../cmake/iotsdk_linux
 make
 
 sudo make install
-'
+
+#Pop out of cmake dir
+popd
+
+#Pop out of iotsdk build dir
+popd
+
+#Pop out of sdk dir and back to sample dir
+popd
+
 
 vercomp() {
     if [[ $1 == $2 ]]
@@ -71,7 +90,7 @@ else
     echo "cmake version check pass (current:$CMAKE_VER,require:$CMAKE_LEAST)"
 fi
 
-git clone git://git.drogon.net/wiringPi
+git clone https://github.com/WiringPi/WiringPi.git wiringPi
 cd ./wiringPi
 ./build
 cd ..
